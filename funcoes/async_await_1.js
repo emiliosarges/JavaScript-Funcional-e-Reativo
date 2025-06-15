749def25b3e2f065401525e083dd0baea6c1da95
Emílio Silva <emiliosarges@gmail.com>
2025-06-15 19:09:28 -0300

    Async/Await e O.O
    Entendendo sobre funções no contexto de O.O

--- funcoes/async_await_1.js
+++ funcoes/async_await_1.js
@@ -1,29 +1,36 @@
 function esperarPor(tempo = 2000) {
-    return new Promise(function(resolve) {
-        setTimeout(() => resolve(), tempo)
-    });
+  return new Promise(function (resolve) {
+    setTimeout(() => resolve(), tempo);
+  });
 }
 
 function retornarValor() {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(10), 5000)
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(10), 5000);
+  });
 }
 
 async function executar() {
-    let valor = await retornarValor()
+  let valor = await retornarValor();
 
-    await esperarPor(1500)
-    console.log(`Async/Await ${valor}`)
+  await esperarPor(1500);
+  console.log(`Async/Await ${valor}`);
 
-    await esperarPor(1500)
-    console.log(`Async/Await ${valor + 1}`)
+  await esperarPor(1500);
+  console.log(`Async/Await ${valor + 1}`);
 
-    await esperarPor(1500)
-    console.log(`Async/Await ${valor + 2}`)
+  await esperarPor(1500);
+  console.log(`Async/Await ${valor + 2}`);
 
-    return valor + 3
+  return valor + 3;
 }
 
-executar()
-    .then(console.log)
+async function executarDeVerdade() {
+  const valor = await executar();
+  console.log(valor);
+}
+
+executarDeVerdade();
+
+// executar()       //Função asincrona retorna uma Promise, por isso precisa do chamr o then
+//     .then(console.log)
